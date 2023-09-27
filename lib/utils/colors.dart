a62ec4e52573241192d82bf6339bf295cd49a567
Lans12345 <olanalans12345@gmail.com>
2023-09-27 17:41:04 +0800

    added project structure

---
+++ lib/utils/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+var primary = const Color.fromARGB(255, 139, 223, 143);
---
+++ lib/widgets/drawer_widget.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_force/widgets/text_widget.dart';
+
+import '../utils/colors.dart';
+
+class DrawerWidget extends StatefulWidget {
+  const DrawerWidget({Key? key}) : super(key: key);
+
+  @override
+  State<DrawerWidget> createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<DrawerWidget> {
+  // final Stream<DocumentSnapshot> userData =
+  //     FirebaseFirestore.instance.collection('Users').doc(userId).snapshots();
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 220,
+      child: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.only(top: 0),
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: primary,
+              ),
+              accountEmail:
+                  TextWidget(text: '', fontSize: 12, color: Colors.white),
+              accountName: TextWidget(
+                text: '',
+                fontSize: 14,
+                color: Colors.white,
+              ),
+              currentAccountPicture: const Padding(
+                padding: EdgeInsets.all(5.0),
+                child: CircleAvatar(
+                  minRadius: 50,
+                  maxRadius: 50,
+                  backgroundImage: AssetImage('assets/images/profile.png'),
+                ),
+              ),
+            ),
+            ListTile(
+              title: TextWidget(
+                text: 'Home',
+                fontSize: 12,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                //     builder: (context) => const MainHomeScreen()));
+              },
+            ),
+            ListTile(
+              title: TextWidget(
+                text: 'About Us',
+                fontSize: 12,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                //     builder: (context) => const MainHomeScreen()));
+              },
+            ),
+            ListTile(
+              title: TextWidget(
+                text: 'Contact Us',
+                fontSize: 12,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                //     builder: (context) => const MainHomeScreen()));
+              },
+            ),
+            ListTile(
+              title: TextWidget(
+                text: 'Logout',
+                fontSize: 12,
+                color: Colors.black,
+              ),
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: const Text(
+                            'Logout Confirmation',
+                            style: TextStyle(
+                                fontFamily: 'QBold',
+                                fontWeight: FontWeight.bold),
+                          ),
+                          content: const Text(
+                            'Are you sure you want to Logout?',
+                            style: TextStyle(fontFamily: 'QRegular'),
+                          ),
+                          actions: <Widget>[
+                            MaterialButton(
+                              onPressed: () => Navigator.of(context).pop(true),
+                              child: const Text(
+                                'Close',
+                                style: TextStyle(
+                                    fontFamily: 'QRegular',
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            MaterialButton(
+                              onPressed: () async {
+                                // Navigator.of(context).pushReplacement(
+                                //     MaterialPageRoute(
+                                //         builder: (context) =>
+                                //             const LoginScreen()));
+                              },
+                              child: const Text(
+                                'Continue',
+                                style: TextStyle(
+                                    fontFamily: 'QRegular',
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
