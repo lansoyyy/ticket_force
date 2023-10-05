6a3b0e920d38519e8e366f0f48d6b996c1c052d7
Lans12345 <olanalans12345@gmail.com>
2023-10-05 16:40:42 +0800

    wip drivers tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_force/screens/tabs/drivers_tab.dart';
 import 'package:ticket_force/utils/colors.dart';
 import 'package:ticket_force/widgets/drawer_widget.dart';
 
@@ -17,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final tabs = [
-      const SizedBox(),
+      const DriversTab(),
       const SizedBox(),
       const SizedBox(),
     ];
