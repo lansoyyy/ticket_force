507fc81cf8164b3c68a2e3e0b58af155102fb805
Lans12345 <olanalans12345@gmail.com>
2023-10-05 16:50:46 +0800

    added drivers tab ui

--- lib/screens/tabs/drivers_tab.dart
+++ lib/screens/tabs/drivers_tab.dart
@@ -1,10 +1,210 @@
 import 'package:flutter/material.dart';
+import 'package:ticket_force/widgets/text_widget.dart';
 
-class DriversTab extends StatelessWidget {
+class DriversTab extends StatefulWidget {
   const DriversTab({super.key});
 
   @override
+  State<DriversTab> createState() => _DriversTabState();
+}
+
+class _DriversTabState extends State<DriversTab> {
+  final searchController = TextEditingController();
+  String nameSearched = '';
+  @override
   Widget build(BuildContext context) {
-    return const Column();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/profile.png',
+                          height: 100,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextWidget(
+                          text: 'Spencer So',
+                          fontSize: 24,
+                          fontFamily: 'Bold',
+                          color: Colors.black,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextWidget(
+                          text: '2020300527',
+                          fontSize: 14,
+                          fontFamily: 'Medium',
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextWidget(
+                          text: '45 TICKETS',
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontFamily: 'Bold',
+                        ),
+                        TextWidget(
+                          text: 'TICKETFORCE',
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontFamily: 'Bold',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextWidget(
+                      text: 'Drivers',
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontFamily: 'Bold',
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Container(
+                        height: 40,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white,
+                            ),
+                            borderRadius: BorderRadius.circular(100)),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: TextFormField(
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Regular',
+                                fontSize: 14),
+                            onChanged: (value) {
+                              setState(() {
+                                nameSearched = value;
+                              });
+                            },
+                            decoration: const InputDecoration(
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                ),
+                                hintText: 'Search Name/Licensed Number',
+                                hintStyle: TextStyle(fontFamily: 'QRegular'),
+                                suffixIcon: Icon(
+                                  Icons.search,
+                                  color: Colors.grey,
+                                )),
+                            controller: searchController,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: 130,
+                      child: ListView.builder(
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: Image.asset('assets/images/profile.png'),
+                            title: TextWidget(
+                              text: 'John Doe',
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontFamily: 'Bold',
+                            ),
+                            subtitle: TextWidget(
+                              text: '2020300527',
+                              fontSize: 12,
+                              color: Colors.grey,
+                              fontFamily: 'Regular',
+                            ),
+                            trailing: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.check_box,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextWidget(
+                          text: 'Driver not registered?',
+                          fontSize: 12,
+                          color: Colors.grey,
+                          fontFamily: 'Regular',
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: TextWidget(
+                            text: 'CLICK HERE',
+                            fontSize: 12,
+                            color: Colors.blue,
+                            fontFamily: 'Bold',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
