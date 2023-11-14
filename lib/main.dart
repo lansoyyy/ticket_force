0e0ff6fcac5f3ceb45e55110ce8514036e88926f
Lans12345 <olanalans12345@gmail.com>
2023-11-14 16:37:03 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_force/firebase_options.dart';
 import 'package:ticket_force/screens/landing_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'ticketforce-5ac87',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
